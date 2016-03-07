cb77a173ea45a20d735d45d949f90a71c00c6a1a
Paul Davies <puddlewitt@hotmail.com>
2016-03-07 12:35:14 +0000

    Initial checkin.

---
+++ manual/findandreplace/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'findandreplace'
+$url = 'http://heanet.dl.sourceforge.net/project/findandreplace/findandreplace/2.0/FAR-2.0-win.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
---
+++ manual/findandreplace/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'findandreplace'
+$filename = 'FAR-2.0-win.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$filename"
