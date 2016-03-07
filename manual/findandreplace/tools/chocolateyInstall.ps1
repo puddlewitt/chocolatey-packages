14b3e0ea2f1f4c8f414f6347ac2c2a4c82f587f4
Paul Davies <puddlewitt@hotmail.com>
2016-03-07 13:31:47 +0000

    Package creation

--- manual/findandreplace/tools/chocolateyInstall.ps1
+++ manual/findandreplace/tools/chocolateyInstall.ps1
@@ -1,4 +1,9 @@
 ﻿$packageName = 'findandreplace'
 $url = 'http://heanet.dl.sourceforge.net/project/findandreplace/findandreplace/2.0/FAR-2.0-win.zip'
+$location = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutFilePath = Join-Path -path $location -childpath 'far.ink'
+$targetPath = Join-Path -path $location -childpath 'far.exe'
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage "$packageName" "$url" "$location"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
