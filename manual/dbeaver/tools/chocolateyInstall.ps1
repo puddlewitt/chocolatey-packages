69930fec6b41e8e14e154b2a6c0e2e9e5aff7949
unknown <davies_pa@UKEHHWW-8Q7L8X1.PEROOT.COM>
2016-03-31 16:07:47 +0100

    dbeaver init

---
+++ manual/dbeaver/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'findandreplace'
+$url = 'https://sourceforge.net/projects/findandreplace/files/findandreplace/2.0/FAR-2.0-win.zip'
+$location = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$desktop = [Environment]::GetFolderPath("Desktop")
+$shortcutFilePath = Join-Path -path $desktop -childpath 'far.lnk'
+$execDir = Join-Path -path $location -childpath 'far'
+$targetPath = Join-Path -path $execDir -childpath 'far.exe'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$location"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath -workingDirectory $execDir
---
+++ manual/dbeaver/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'findandreplace'
+$filename = 'FAR-2.0-win.zip'
+$desktop = [Environment]::GetFolderPath("Desktop")
+$shortcutFilePath = Join-Path -path $desktop -childpath 'far.lnk'
+
+Uninstall-ChocolateyZipPackage "$packageName" "$filename"
+
+Remove-Item $shortcutFilePath
